44722d6f283aac87953b3cae615ebd917000e4bf
Rachel <80181684+rappleit@users.noreply.github.com>
2023-05-30 01:24:07 +0800

    transferred web-app code from old repo

---
+++ web-app/src/pages/timer.js
@@ -0,0 +1,17 @@
+import styles from '@/styles/Timer.module.css'
+
+const Timer = () => {
+    return (
+        <>
+            <div className={styles.header}>
+                <div className={styles.logo}>
+                    <img src="../assets/logo.svg" alt=""/>
+                    <p>Stretch925</p>
+                </div>
+                
+            </div>
+        </>
+    );
+}
+
+export default Timer;
