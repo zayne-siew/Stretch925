1a1638dbf291faf474a79f1815b89234589d5f2c
Rachel <80181684+rappleit@users.noreply.github.com>
2023-05-30 04:07:32 +0800

    implemented break time screen

--- web-app/src/components/breakTime.js
+++ web-app/src/components/breakTime.js
@@ -1,6 +1,20 @@
-const BreakTime = () => {
-    return (<>
-    </>);
+import styles from '@/styles/BreakTime.module.css'
+
+
+const BreakTime = ({
+    numSessions,
+    breakSecondsLeft
+}) => {
+    return (
+        <>
+            <p>Number of sessions left: {numSessions}</p>
+            <p className={styles.breakTimer}>Break time left: {Math.floor(breakSecondsLeft/60)}:{(breakSecondsLeft - (Math.floor(breakSecondsLeft/60) * 60) < 10) ? 0:<></>}{breakSecondsLeft - (Math.floor(breakSecondsLeft/60) * 60)}</p>
+
+            <h1 className={styles.header}>BREAK<br/>TIME</h1>
+            <button className={styles.stretchButton}>STRETCH!!!</button>
+            
+        </>
+    );
 }
 
 export default BreakTime;
--- web-app/src/pages/timer.js
+++ web-app/src/pages/timer.js
@@ -1,3 +1,4 @@
+import BreakTime from '@/components/breakTime';
 import Pomodoro from '@/components/pomodoro';
 import styles from '@/styles/Timer.module.css'
 import { useEffect, useState } from 'react';
@@ -9,9 +10,11 @@ const Timer = () => {
     const [timer, setTimer] = useState();
     const [numSessions, setNumSessions] = useState(4);
 
-    const [isBreak, setIsBreak] = useState(false)
+    const [isBreak, setIsBreak] = useState(false);
     const [breakTimer, setBreakTimer] = useState();
     const [breakSecondsLeft, setBreakSecondsLeft] = useState(300);
+
+    const [isStretch, setIsStretch] = useState(false);
 
 
     const startTimer = () => {
@@ -35,6 +38,8 @@ const Timer = () => {
             setIsTimerActive(false);
             setSecondsLeft(originalSeconds);
             setNumSessions(numSessions - 1);
+            setIsBreak(true);
+            startBreakTimer();
         } else if (secondsLeft === 0 && numSessions === 0) {
             clearInterval(timer);
             setIsTimerActive(false);
@@ -42,9 +47,25 @@ const Timer = () => {
             setNumSessions(4);
         }
     }, [secondsLeft, timer]);
+
     useEffect(() => {
         return () => clearInterval(timer);
     }, [timer]);
+
+    const startBreakTimer = () => {
+        const timer = setInterval(() => {
+            setBreakSecondsLeft((breakSecondsLeft) => breakSecondsLeft - 1);
+        }, 1000);
+        setBreakTimer(timer);
+    }
+
+    useEffect(() => {
+        if (breakSecondsLeft === 0) {
+            clearInterval(breakTimer);
+            setIsBreak(false);
+            setBreakSecondsLeft(300);
+        }
+    }, [breakSecondsLeft, breakTimer])
 
 
     return (
@@ -57,7 +78,11 @@ const Timer = () => {
                     </div>
                     <div className={styles.divider}></div>
                 </div>
-                <Pomodoro {...{ isTimerActive, setIsTimerActive, secondsLeft, setSecondsLeft, startTimer, stopTimer, numSessions }} />
+                {
+                    (!isBreak) ?
+                    <Pomodoro {...{ isTimerActive, setIsTimerActive, secondsLeft, setSecondsLeft, startTimer, stopTimer, numSessions }} /> :
+                    <BreakTime {...{numSessions, breakSecondsLeft}}/>
+                }
             </div>
         </>
     );
